b77ce95e4d9196d5e959fa8dc7e8c1d14fa4ec27
ThiagoTheodoro <40611427+ThiagoCode96@users.noreply.github.com>
2023-01-12 12:28:05 -0300

    Adicionando IBGE e refinamento de algumas classes

--- Database/Telephone.cs
+++ Database/Telephone.cs
@@ -11,15 +11,11 @@ namespace PesquisaEstados.Database
 {
     class Telephone
     {
-        private string sigla { get; set; }
-        private string cidade { get; set; }
-        private string estado { get; set; }
-        private string ddd { get; set; }
-
         public static DataTable StartList(bool active)
         {
             var dt = new DataTable();
-            var querry = "select estado.uf as \"Sigla\", estado.nome as \"Estado\" , cidade.nome as Cidade, estado.ddd as \"ddd\" from cidade INNER \r\nJOIN estado on cidade.uf=estado.id";
+            var querry = $"select  cidade.nome as \"Cidade\", estado.nome as \"Estado\" , estado.uf as \"Sigla\",  estado.ddd as \"ddd\", cidade.ibge as \"IBGE\"  from cidade INNER JOIN estado on " +
+                $"cidade.uf=estado.id";
 
             try
             {
@@ -41,10 +37,11 @@ namespace PesquisaEstados.Database
         public static DataTable GetList(string cidade, string estado,string sigla, string ddd)
         {
             var dt = new DataTable();
-            var querry = $"select estado.uf as \"Sigla\", estado.nome as \"Estado\" , cidade.nome as Cidade, estado.ddd as \"ddd\" from cidade INNER JOIN estado on " +
+            var querry = $"select  cidade.nome as \"Cidade\", estado.nome as \"Estado\" , estado.uf as \"Sigla\",  estado.ddd as \"ddd\", cidade.ibge as \"IBGE\" from cidade INNER JOIN estado on " +
                 $"cidade.uf=estado.id and " +
                 $"cidade.nome like '%{cidade}%'and " +
-                $"estado.nome like '%{estado}%'and estado.uf like'%{sigla}%'" +
+                $"estado.nome like '%{estado}%'and " +
+                $"estado.uf like'%{sigla}%'" +
                 $"and estado.ddd like '%{ddd}%'";
 
             try
